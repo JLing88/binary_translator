2610eb52c8c02fef99fbcd0b46acdbf38d5d33ff
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-06 08:52:57 -0600

    Adds translate to text method

--- lib/binary_translator.rb
+++ lib/binary_translator.rb
@@ -3,6 +3,8 @@ class BinaryTranslator
   attr_accessor :alpha_to_binary
 
   def initialize
+
+    @alpha_to_binary = Hash.new("")
     @alpha_to_binary = {
       "a" => "000001",
       "b" => "000010",
@@ -40,5 +42,15 @@ class BinaryTranslator
       @alpha_to_binary[char.downcase]
     end
     char_array.join
+  end
+
+  def translate_to_text(string)
+    new_hash = @alpha_to_binary.invert
+    new_array = string.scan(/.{6}/)
+    new_array.map! do |binary|
+      new_hash[binary]
+    end
+    new_array.join
+
   end
 end
